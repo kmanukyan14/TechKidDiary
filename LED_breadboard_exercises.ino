0377b41fd57cf92f8ac727cdda43419d5547730e
kmanukyan14 <136882162+kmanukyan14@users.noreply.github.com>
2023-06-16 23:45:42 -0700

    Add files via upload

---
+++ LED_breadboard_exercises.ino
@@ -0,0 +1,13 @@
+void setup() {
+pinMode(13,OUTPUT);
+pinMode(12,OUTPUT);
+}
+
+void loop() {
+digitalWrite(13,HIGH);
+digitalWrite(12,LOW);
+delay(500);
+digitalWrite(12,HIGH);
+digitalWrite(13,LOW);
+delay(500);
+}
