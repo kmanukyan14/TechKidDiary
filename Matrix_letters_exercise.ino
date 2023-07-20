c066ddd62ac8274490a3f67877d51f2d798bc1c9
kmanukyan14 <136882162+kmanukyan14@users.noreply.github.com>
2023-07-20 11:29:27 -0700

    Matrix_numbers.ino

--- Matrix_letters_exercise.ino
+++ Matrix_letters_exercise.ino
@@ -7,40 +7,16 @@ void setup() {
   lc.clearDisplay(0);
 }
 
-void A() {
+void num0() {
   lc.clearDisplay(0);
-  lc.setRow(0,3,B00111111);
-  lc.setRow(0,4,B01000100);
-  lc.setRow(0,5,B01000100);
-  lc.setRow(0,6,B01000100);
-  lc.setRow(0,7,B00111111);
-  delay(500);
-}
-
-void r() {
-  lc.clearDisplay(0);
-  lc.setRow(0,3,B00001000);
-  lc.setRow(0,4,B00010000);
-  lc.setRow(0,5,B00010000);
-  lc.setRow(0,6,B00001000);
-  lc.setRow(0,7,B00011111);
-  delay(500);
-}
-
-void m() {
-  lc.clearDisplay(0);
-  lc.setRow(0,1,B00001111);
-  lc.setRow(0,2,B00010000);
-  lc.setRow(0,3,B00010000);
-  lc.setRow(0,4,B00001111);
-  lc.setRow(0,5,B00010000);
-  lc.setRow(0,6,B00010000);
-  lc.setRow(0,7,B00011111);
+  lc.setRow(0,3,B00111000);
+  lc.setRow(0,4,B00101000);
+  lc.setRow(0,5,B00101000);
+  lc.setRow(0,6,B00101000);
+  lc.setRow(0,7,B00111000);
   delay(500);
 }
 
 void loop() { 
-A();
-r();
-m();
+num0();
 }
