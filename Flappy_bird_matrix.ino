4440efeb3d21fdf925f2904046f2fac62b01d95f
kmanukyan14 <136882162+kmanukyan14@users.noreply.github.com>
2023-09-02 16:09:48 -0700

    Flappy_bird_matrix.ino

--- Flappy_bird_matrix.ino
+++ Flappy_bird_matrix.ino
@@ -4,6 +4,7 @@ void setup() {
   lc.shutdown(0,false);
   lc.setIntensity(0,8);
   randomSeed(analogRead(2));
+  Serial.begin(9600); //new
 }
 int j, dot, height, width;
 void line(){
@@ -11,6 +12,10 @@ void line(){
   lc.setLed(0,7,dot,true);
   for(int i=0; i<=height; i++)lc.setLed(0,j,i,true);
   for(int i=height+width; i<=7; i++)lc.setLed(0,j,i,true);
+}
+int score = 0; //new
+void animation(){
+//write your animation here
 }
 void loop(){
   height = random(4); 
@@ -20,6 +25,8 @@ void loop(){
   line();
   delay(200);
   if(j==7 && (dot<=height || dot>=height+width)){
-    lc.clearDisplay(0); delay(2000);
+    lc.clearDisplay(0); Serial.println(score); score = 0; animation(); delay(2000);
   } 
-}} 
+}
+score += 1; //new
+} 
