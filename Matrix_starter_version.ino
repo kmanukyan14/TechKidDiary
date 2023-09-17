9a1d2cef78ad52354ade5f9724a034209aede176
kmanukyan14 <136882162+kmanukyan14@users.noreply.github.com>
2023-09-17 09:22:05 -0700

    Add files via upload

---
+++ Matrix_starter_version.ino
@@ -0,0 +1,10 @@
+#include "LedControl.h"
+LedControl lc=LedControl(12,10,11,1);
+void setup() {
+  lc.shutdown(0,false);
+  lc.setIntensity(0,8);
+  lc.clearDisplay(0);
+}
+void loop(){
+lc.setLed(0, 3, 3, true);
+}
